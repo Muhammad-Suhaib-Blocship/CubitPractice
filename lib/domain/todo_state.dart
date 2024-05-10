f78c2811bc145439f6395bc3ce770fead75ae721
Suhaibusman <suhaibusman54@gmail.com>
2024-05-10 18:47:43 +0500

    freezed added

--- lib/domain/todo_state.dart
+++ lib/domain/todo_state.dart
@@ -1,30 +1,53 @@
 import 'package:cubitpractice/models/todo_model.dart';
 
+/// state => initial state, loading state, loaded state, error state
+//
+// abstract class PRState {}
+//
+// class InitialState extends PRState {
+//   InitialState();
+// }
+//
+// class LoadingState extends PRState {
+//   LoadingState();
+// }
+//
+// class LoadedState extends PRState {
+//   LoadedState();
+// }
+//
+// class ErrorState extends PRState {
+//   ErrorState();
+// }
+//
+// class PRCubit extends Cubit<PRState> {
+//   PRCubit() : super(InitialState());
+//
+//   void getTodos() {
+//     emit(LoadingState());
+//     try {
+//       // api call
+//       emit(LoadedState());
+//     } catch (e) {
+//       emit(ErrorState());
+//     }
+//
+//     emit(LoadedState());
+//     // final tempList = [...state.todos];
+//     // tempList.add(ToDoModel(name: name, email: email));
+//     // // tempList.add(email);
+//     // print(tempList);
+//     // emit(state.copyWith(todos: tempList));
+//   }
+// }
+
 class TodoState {
-
   final List<ToDoModel> todos;
-  TodoState({
-    // required this.name,
-    // required this.email,
-    required this.todos,
-  });
 
-  factory TodoState.initial() => TodoState(
-        // name: "",
-        // email: "",
-        todos: [],
-      );
+  TodoState({required this.todos});
 
-  TodoState copyWith({
-    // String? name,
-    // String? email,
-    List<ToDoModel>? todos,
-  }) =>
-      TodoState(
-        // name: name ?? this.name,
-        // email: email ?? this.email,
-        todos: todos??this.todos,
-      );
+  factory TodoState.initial() => TodoState(todos: []);
 
-
+  TodoState copyWith({List<ToDoModel>? todos}) =>
+      TodoState(todos: todos ?? this.todos);
 }
--- lib/view/counter_view.dart
+++ lib/view/counter_view.dart
@@ -1,4 +1,5 @@
 import 'package:cubitpractice/cubit/counter_cubit.dart';
+import 'package:cubitpractice/domain/todo_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,6 +14,50 @@ class CounterView extends StatelessWidget {
       create: (context) => CounterCubit(),
       child: BlocBuilder<CounterCubit, int>(
         builder: (context, state) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$state',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                context.read<CounterCubit>().increment();
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PRView extends StatelessWidget {
+  const PRView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: BlocBuilder<CounterCubit, int>(
+        builder: (context, state) {
+          // if (state == LoadingState()) {}
+          //
+          // if (state == LoadedState()) {}
+
           return Scaffold(
             body: Center(
               child: Column(
