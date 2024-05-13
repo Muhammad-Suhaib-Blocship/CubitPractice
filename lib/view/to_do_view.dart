c71105fa5017c0e6eb05914fe1dbc294b7159d12
Suhaibusman <suhaibusman54@gmail.com>
2024-05-13 11:17:21 +0500

    freezed done

--- lib/view/to_do_view.dart
+++ lib/view/to_do_view.dart
@@ -19,129 +19,133 @@ class TodoView extends StatelessWidget {
       create: (context) => todoCubit,
       child: BlocBuilder<ToDoCubit, TodoState>(
         builder: (context, state) {
-          return Scaffold(
-            body: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: nameController,
-                    maxLines: 1,
-                    decoration: const InputDecoration(
-                        hintText: "Name", border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    controller: emailController,
-                    maxLines: 1,
-                    decoration: const InputDecoration(
-                        hintText: "Email", border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        context.read<ToDoCubit>().addTodos(
-                            name: nameController.text,
-                            email: emailController.text);
-                        nameController.clear();
-                        emailController.clear();
-                      },
-                      child: const Text("Add")),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: state.todos.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(state.todos[index].name),
-                          subtitle: Text(state.todos[index].email),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                icon: const Icon(Icons.edit),
-                                onPressed: () {
-                                  updateNameController.text =
-                                      state.todos[index].name;
-
-                                  updateEmailController.text =
-                                      state.todos[index].email;
-                                  showDialog(
-                                    context: context,
-                                    builder: (ctx) {
-                                      return BlocProvider.value(
-                                          value: todoCubit,
-                                          child: AlertDialog(
-                                            title: const Text("Update"),
-                                            content: Column(
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                TextField(
-                                                  controller:
-                                                      updateNameController,
-                                                  maxLines: 1,
-                                                  decoration: const InputDecoration(
-                                                      hintText: "Name",
-                                                      border:
-                                                          OutlineInputBorder()),
-                                                ),
-                                                const SizedBox(
-                                                  height: 20,
-                                                ),
-                                                TextField(
-                                                  controller:
-                                                      updateEmailController,
-                                                  maxLines: 1,
-                                                  decoration: const InputDecoration(
-                                                      hintText: "Email",
-                                                      border:
-                                                          OutlineInputBorder()),
-                                                ),
-                                              ],
-                                            ),
-                                            actions: [
-                                              ElevatedButton(
-                                                  onPressed: () {
-                                                    context
-                                                        .read<ToDoCubit>()
-                                                        .updateTodos(
-                                                            index: index,
-                                                            name:
-                                                                updateNameController
-                                                                    .text,
-                                                            email:
-                                                                updateEmailController
-                                                                    .text);
-                                                    Navigator.pop(context);
-                                                  },
-                                                  child: const Text("Update"))
-                                            ],
-                                          ));
-                                    },
-                                  );
-                                },
-                              ),
-                              IconButton(
-                                icon: const Icon(Icons.delete),
-                                onPressed: () {
-                                  context
-                                      .read<ToDoCubit>()
-                                      .deleteTodos(index: index);
-                                },
-                              ),
-                            ],
-                          ),
-                        );
-                      },
+          return SafeArea(
+            child: Scaffold(
+              body: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      maxLines: 1,
+                      decoration: const InputDecoration(
+                          hintText: "Name", border: OutlineInputBorder()),
                     ),
-                  )
-                ],
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      controller: emailController,
+                      maxLines: 1,
+                      decoration: const InputDecoration(
+                          hintText: "Email", border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          context.read<ToDoCubit>().addTodos(
+                              name: nameController.text,
+                              email: emailController.text);
+                          nameController.clear();
+                          emailController.clear();
+                        },
+                        child: const Text("Add")),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: state.todos.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(state.todos[index].name),
+                            subtitle: Text(state.todos[index].email),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () {
+                                    updateNameController.text =
+                                        state.todos[index].name;
+
+                                    updateEmailController.text =
+                                        state.todos[index].email;
+                                    showDialog(
+                                      context: context,
+                                      builder: (ctx) {
+                                        return BlocProvider.value(
+                                            value: todoCubit,
+                                            child: AlertDialog(
+                                              title: const Text("Update"),
+                                              content: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  TextField(
+                                                    controller:
+                                                        updateNameController,
+                                                    maxLines: 1,
+                                                    decoration:
+                                                        const InputDecoration(
+                                                            hintText: "Name",
+                                                            border:
+                                                                OutlineInputBorder()),
+                                                  ),
+                                                  const SizedBox(
+                                                    height: 20,
+                                                  ),
+                                                  TextField(
+                                                    controller:
+                                                        updateEmailController,
+                                                    maxLines: 1,
+                                                    decoration:
+                                                        const InputDecoration(
+                                                            hintText: "Email",
+                                                            border:
+                                                                OutlineInputBorder()),
+                                                  ),
+                                                ],
+                                              ),
+                                              actions: [
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      context
+                                                          .read<ToDoCubit>()
+                                                          .updateTodos(
+                                                              index: index,
+                                                              name:
+                                                                  updateNameController
+                                                                      .text,
+                                                              email:
+                                                                  updateEmailController
+                                                                      .text);
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: const Text("Update"))
+                                              ],
+                                            ));
+                                      },
+                                    );
+                                  },
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () {
+                                    context
+                                        .read<ToDoCubit>()
+                                        .deleteTodos(index: index);
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
